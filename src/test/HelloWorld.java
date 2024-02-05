d852416686876bbfb81a5223e1224d83091d8a3e
tranduc312 <39908814+tranduc312@users.noreply.github.com>
2024-02-05 15:27:59 +0700

    test jenkins poll SCM

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -2,6 +2,6 @@ package test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld by jenkins updatedddd");
+        System.out.println("HelloWorld by jenkins updateddddd");
     }
 }
